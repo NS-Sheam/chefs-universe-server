5443302bda613167616e099bea4e553adb4222e0
NS-Sheam <123sheamfeni@gmail.com>
2023-05-02 11:58:38 +0600

    server almost finished with 2 type of load data

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const chefData = require("./data/chefData.json");
+const recipeData = require("./data/recipeData.json");
+
+app.use(cors());
 
 
+// load chef data 
 app.get("/", (req, res) => {
-    res.send("recipe hunting is running")
+    res.send(chefData);
 })
+
+// load recipe Data 
+app.get("/:id", (req, res) =>{
+    const id = req.params.id;
+    const filterRecipe = recipeData.filter(recipe => recipe.recipeId === +id);
+    res.send(filterRecipe);
+})
+
 
 app.listen(port, () => {
     console.log(`recipe hunting is running on port ${port}`);
